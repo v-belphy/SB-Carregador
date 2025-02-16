03e89584b7efd4c309749063d3898ad71ba32da7
Belphy <belphylain@gmail.com>
2025-02-16 16:40:01 -0300

    Projeto implementado! :)

--- carregador.asm
+++ carregador.asm
@@ -1,6 +1,6 @@
 SECTION .data
 
-MSG_NAO_CABE db "O programa nao cabe em memoria.", 0xA
+MSG_NAO_CABE db "O programa nao cabe em memoria."
 MSG_NAO_CABE_SIZE EQU $-MSG_NAO_CABE
 MSG_CABE db "O programa cabe na memoria e está localizado no:"
 MSG_CABE_SIZE EQU $-MSG_CABE
@@ -10,6 +10,7 @@ MSG_ENDERECO_INICIAL db ": endereço inicial = "
 MSG_ENDERECO_INICIAL_SIZE EQU $-MSG_ENDERECO_INICIAL
 MSG_ENDERECO_FINAL db " | endereco final = "
 MSG_ENDERECO_FINAL_SIZE EQU $-MSG_ENDERECO_FINAL
+MSG_ENDL db 0xA
 
 SECTION .bss
 
@@ -52,21 +53,30 @@ cmp PROG_SIZE, EAX
 jg nao_cabe
 
 ;CASO 2) Programa cabe inteiramente em algum bloco
-loop_1:
+push MSG_CABE_SIZE
+push MSG_CABE
+call print_string
+
 mov ECX, 0 ;index
-mov EBX, EBP+12 ;base endereco
-mov EDX, EBP+16 ;base tamanho
-cmp [EDX + ECX * 8], PROG_SIZE
+loop_1:
+mov EBX, EBP 
+add EBX, 12 ;base endereco
+mov EDX, EBP
+add EDX, 16 ;base tamanho
+mov EAX, PROG_SIZE
+cmp [EDX + ECX * 8], EAX
 jge cabe
 inc ECX
 cmp ECX, 4
 jl loop_1
 
 ;CASO 3) Programa cabe divido em blocos
-loop_2:
 mov ECX, 0 ;index
-mov EBX, EBP+12 ;base endereco do bloco
-mov EDX, EBP+16 ;base tamanho do bloco
+loop_2:
+mov EBX, EBP 
+add EBX, 12 ;base endereco
+mov EDX, EBP
+add EDX, 16 ;base tamanho
 jmp cabe
 CONTINUA_LOOP:
 inc ECX
@@ -78,51 +88,52 @@ jmp carrega_fim
 ;Coloca o programa no bloco, setando os resultados: BLOCO_IDX, ENDERECO_INICIAL e ENDERECO_FINAL
 cabe:
 mov [BLOCO_IDX], ECX
-inc [BLOCO_IDX]
-mov EAX, [EBX + ECX * 4]
+inc DWORD [BLOCO_IDX]
+mov EAX, [EBX + ECX * 8]
 mov [ENDERECO_INICIAL], EAX
 mov EAX, [ENDERECO_INICIAL]
 mov [ENDERECO_FINAL], EAX
-mov EAX, [EDX + ECX * 4] 
+mov EAX, [EDX + ECX * 8] 
 cmp EAX, PROG_SIZE
 jl auxiliar
 mov EAX, PROG_SIZE
 auxiliar:
 add [ENDERECO_FINAL], EAX
-dec [ENDERECO_FINAL]
+dec DWORD [ENDERECO_FINAL]
 
 ;chama a funcao que converte int para string e depois chama a funcao para printar o resultado e MSG correspondente
-push [BLOCO_IDX]
+push DWORD [BLOCO_IDX]
 push BLOCO_IDX_STR
 call int_to_string
-push [MSG_BLOCO_SIZE]
+push DWORD MSG_BLOCO_SIZE
 push MSG_BLOCO
 call print_string
-push [TAMANHO_DA_MSG]
+push DWORD [TAMANHO_DA_MSG]
 push BLOCO_IDX_STR
 call print_string
 
-push [ENDERECO_INICIAL]
+push DWORD [ENDERECO_INICIAL]
 push ENDERECO_INICIAL_STR
 call int_to_string
-push [MSG_ENDERECO_INICIAL_SIZE]
+push DWORD MSG_ENDERECO_INICIAL_SIZE
 push MSG_ENDERECO_INICIAL
 call print_string
-push [TAMANHO_DA_MSG]
+push DWORD [TAMANHO_DA_MSG]
 push ENDERECO_INICIAL_STR
 call print_string
 
-push [ENDERECO_FINAL]
+push DWORD [ENDERECO_FINAL]
 push ENDERECO_FINAL_STR
 call int_to_string
-push [MSG_ENDERECO_FINAL_SIZE]
+push DWORD MSG_ENDERECO_FINAL_SIZE
 push MSG_ENDERECO_FINAL
-push [TAMANHO_DA_MSG]
+call print_string
+push DWORD [TAMANHO_DA_MSG]
 push ENDERECO_FINAL_STR
 call print_string
 
 ;verifica se ainda há programa para ser armazenado, se sim, continua o LOOP
-mov EAX, [EDX + ECX * 4]
+mov EAX, [EDX + ECX * 8]
 sub PROG_SIZE, EAX
 jg CONTINUA_LOOP
 jmp carrega_fim
@@ -138,6 +149,9 @@ jmp carrega_fim
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
 carrega_fim:
+push DWORD 1
+push MSG_ENDL
+call print_string
 mov ESP, EBP
 pop EBP
 ret
@@ -181,6 +195,7 @@ ret 8
 print_string:
 push EBP
 mov EBP, ESP
+pusha
 
 mov EAX, 4
 mov EBX, 1
@@ -188,6 +203,7 @@ mov ECX, [EBP+8]
 mov EDX, [EBP+12]
 int 0x80
 
+popa
 mov ESP, EBP
 pop EBP
 ret 8
